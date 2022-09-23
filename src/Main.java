92cd50f26da74fc0b54ce7624bc7486cf5e6fa9b
bekbolotturgunbaev <bekaturgunbaev007@gmail.com>
2022-09-23 22:31:52 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+/*System.out.println(name.charAt(1));//берилген индекстеги символду кайтарып берет
+    String kesuu=name.substring(2);//берилген индекстен баштап созду кесип алат
+System.out.println(kesuu);
+System.out.println(name.contains("r"));//берилген символ создун ичинде ьарбы же жокпу текшерет-> true or false
+System.out.println(name.toUpperCase());//баардык тамганы чог кылып салат
+System.out.println(name.toLowerCase());//баардык тамгаын кичине кылып салат
+System.out.println(soz.trim());//башындагы жана аягындагы боштуктарды алып салат
+System.out.println(soz2.equals(soz));//2 созду бири бирине барабарбы деп текшерет
+System.out.println(name);
+System.out.println(name.replace('a','u'));//создун каалаган тамгасын жаны каалаган тамшага озгортсок болот
+System.out.println(name.length());//создун узундугун чыгарат
+System.out.println(name.concat(" Abdyldaev"));//2 созду бириктирет  */
+    }
+}
